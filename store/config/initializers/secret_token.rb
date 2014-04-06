f3001728763c09a9cc06310b27a3582162986ad8
amirv92 <chainsaw40k@yahoo.com>
2014-04-06 11:33:31 +0430

    added gui combined with rails app

---
+++ store/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'fcde1a79e4aa7428d0ac46a0e105670c0b01c941b7bf557f3537224371eb25a4bc747bfc391fe9ec0e1ee1f4542c427e98ee49527727228c843d35e16ce30122'
