61fd7a0ef70124152bbc7d622cc64d6f98f5ef96
rails <rails@ubuntu.(none)>
2014-04-11 09:27:15 +0430

    fixing the search

--- store/app/helpers/application_helper.rb
+++ store/app/helpers/application_helper.rb
@@ -3,10 +3,10 @@
 module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
-    css_class = (column == sort_column) ? "current &crarr; 
-      #{sort_direction}" : nil
-    direction = (column == sort_column ; 
-      "asc") ? "desc" : "asc"
+    css_class = (column == sort_column) ? "current &crarr; #{sort_direction}" : nil
+   #direction = (column == sort_column ;"asc") ? "desc" : "asc"
+   #direction = (column == sort_column && sort_direction == &crarr;"asc") ? "desc" : "asc"
+    direction = (column == sort_column && sort_direction == "asc" ;) ? "desc" : "asc"   
     link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
   end
 end
